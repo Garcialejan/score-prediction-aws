999d5010c3cb10f92634e2644d2294f3290c6071
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-10-26 11:24:13 +0100

    Logger and exception handler implemented

--- template_generator.py
+++ template_generator.py
@@ -7,18 +7,35 @@ logging.basicConfig(level = logging.INFO,
                     format  ='[%(asctime)s]: %(message)s:')
 
 list_of_files = [
-    f"{project_name}/__init__.py", 
-    f"{project_name}/components/__init__.py", 
-    f"{project_name}/utils/__init__.py", 
-    f"{project_name}/utils/common.py", 
+    f"{project_name}/__init__.py",
+    
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/model_trainer.py",
+    
     f"{project_name}/logging/__init__.py",
+    
     f"{project_name}/config/__init__.py",
     f"{project_name}/config/configuration.py",
+    
     f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/exception/__init__.py", 
-    f"{project_name}/entity/__init__.py", 
-    f"{project_name}/entity/config_entity.py", 
+    f"{project_name}/pipeline/train_pipeline.py",
+    f"{project_name}/pipeline/predict_pipeline.py",
+    
+    f"{project_name}/utils/__init__.py", 
+    f"{project_name}/utils/common.py",
+    
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/exception/exception.py",
+     
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/config_entity.py",
+     
     f"{project_name}/constants/__init__.py",
+    
     f"{project_name}/cloud/__init__.py",
     "Scoring_Data/",
     "notebooks/",
